81de2b8a3d5e6de888ea622fe3a701efb673eb26
quantum.v.rl <quantum.v.rl@gmail.com>
2020-03-07 14:03:54 -0800

    "add ImageScreen and ImageDetail"

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ImageDetail= () => {
+    return(
+        <Text>Show Image of Space</Text>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,22 @@
 import React from "react";
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
-// const HomeScreen = props => {
 const HomeScreen = ({navigation}) => {
 
   return (
     <View>
       <Text style={styles.text}>SupBra</Text>
       <Button
-      // onPress={() => props.navigation.navigate('Components')}
       onPress={() => navigation.navigate('Components')} 
       title="Check out this Component Demo"
       />
       <Button 
         title="Go to List Demo"
-        // onPress={() => props.navigation.navigate('List')}
         onPress={() => navigation.navigate('List')}
+      />
+      <Button 
+        title="Go to List Images"
+        onPress={() => navigation.navigate('Image')}
       />
     </View>
   );
